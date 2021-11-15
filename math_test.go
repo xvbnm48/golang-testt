7c11dd2c58ddcadf0930e0aa1d39c1b7fdb5ed99
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-11-15 21:04:53 +0700

    membuat testing drngan testify

--- math_test.go
+++ math_test.go
@@ -1,12 +1,20 @@
 package go_testing_baru
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestMath(t *testing.T) {
-	c:= Math(-5)
-	 if c != 5 {
-		 t.Logf("expect 5 ,but got %d" ,c)
-		 t.Fail()
-	 }
+	t.Run("negatie test", func(t *testing.T) {
+		c := Math(-5)
+		assert.Equal(t, 5, c)
 
+	})
+	t.Run("testing postivie", func(t *testing.T) {
+		c := Math(5)
+		assert.Equal(t, 5, c)
+
+	})
 }
